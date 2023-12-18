42e9e668f65d29ab0743d9b8b7f58fcbabe4307e
bahti41 <116912093+bahti41@users.noreply.github.com>
2023-12-18 11:56:09 +0300

    düzenleme

--- Bahtiyar_Sonmez_Bright_Akademi_Projesi/README.md
+++ Bahtiyar_Sonmez_Bright_Akademi_Projesi/README.md
@@ -3,7 +3,7 @@ Frontend proje Odevi;
 - "https://baubright.com/" burada yapılan sitenin tasarımını inceledim.
 - Projenin istedildigi gibi klasör oluşumu yaptım.
 - "https://create-react-app.dev/docs/getting-started" sitesinden react app klasörünü indirdim.
-- "https://create-react-app.dev/docs/getting-started" sitesinden bootstrap'ı klasöre indirdim ve "https://react-bootstrap.github.io/docs/getting-started/introduction". sitesinden bootstrap linkimi alıp, public klasörüm icerisindeki index.html dosyama yapıştırdım.
+- "https://create-react-app.dev/docs/getting-started" - "https://react-bootstrap.github.io/docs/getting-started/introduction" - "https://mdbootstrap.com/docs/react/getting-started/installation/" sitelerinden bootstrap, fortawesome, ui-kit in kurulumunu yaptım.
 - Inen klasörde fazlalıkları temizledim.
 - Githup projemi Colobrator yaptım.
 - Loga araştırmalarım.
--- Bahtiyar_Sonmez_Bright_Akademi_Projesi/src/Components/Footer.js
+++ Bahtiyar_Sonmez_Bright_Akademi_Projesi/src/Components/Footer.js
@@ -1,15 +1,82 @@
-import React from 'react'
+import { MDBContainer, MDBFooter, MDBIcon, MDBBtn } from 'mdb-r eact-ui-kit';
+import React from 'react';
 
 
 const Footer = () => {
+
     return (
         <>
-            <footer class="footer">
-                <div class="container text-center">
-                    <span class="text-muted">© 2023
-                        Bright Akademi Website Name</span>
+            <MDBFooter className='bg-light text-center text-white'>
+                <MDBContainer className='p-4 pb-0'>
+                    <section className='mb-4'>
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#3b5998' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='facebook-f' />
+                        </MDBBtn>
+
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#55acee' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='twitter' />
+                        </MDBBtn>
+
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#dd4b39' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='google' />
+                        </MDBBtn>
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#ac2bac' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='instagram' />
+                        </MDBBtn>
+
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#0082ca' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='linkedin-in' />
+                        </MDBBtn>
+
+                        <MDBBtn
+                            floating
+                            className='m-1'
+                            style={{ backgroundColor: '#333333' }}
+                            href='#!'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='github' />
+                        </MDBBtn>
+                    </section>
+                </MDBContainer>
+
+                <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                    © 2023 Copyright:
+                    <a className='text-white' href='https://baubright.com/'>
+                        BAU Bright Akademi
+                    </a>
                 </div>
-            </footer>
+            </MDBFooter>
         </>
     )
 }
