0c1079e61beb420c259d0a8e71a81918636439f1
bati <116912093+bahti41@users.noreply.github.com>
2024-01-16 22:12:54 +0300

    finish

--- Bahtiyar_Sonmez_Bright_Akademi_Projesi/src/Component/Maps.js
+++ Bahtiyar_Sonmez_Bright_Akademi_Projesi/src/Component/Maps.js
@@ -6,7 +6,7 @@ function Maps() {
             <iframe className='map'
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12036.90507342669!2d28.998980830472973!3d41.04218072745107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab7a2a2c3b963%3A0x7671d1b9817b8519!2zQmFow6dlxZ9laGlyIMOcbml2ZXJzaXRlc2k!5e0!3m2!1str!2str!4v1704021485406!5m2!1str!2str"
                 width="100%"
-                height="auto"
+                height="100%"
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
